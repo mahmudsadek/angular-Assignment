e2cd2fbbbd394e0a244e3932ffe23cd44c2da8a1
sadek <mahmoudsadek.1905@gmail.com>
2024-04-02 15:54:59 +0200

    day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    AsideComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
